d35c22b68cfd8a18e6f882d97ade9c43d70133b4
tiagotk360 <contato@tk360.com.br>
2022-09-21 16:10:27 -0300

    3º exercicio

--- bloco-4/dia1/exercicio1.js
+++ bloco-4/dia1/exercicio1.js
@@ -2,6 +2,7 @@ const base = 5;
 const heigth = 8;
 const area = base * heigth;
 
-const perimeter = base + altura;
+const perimeter = base + base + heigth + heigth;
 
 console.log(area);
+console.log(perimeter);
