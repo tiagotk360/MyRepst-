6ab1a0d6d011e93d8ad7a475ae17bb00bcba56a4
tiagotk360 <contato@tk360.com.br>
2022-09-26 13:31:57 -0300

    exercicio fatorial

---
+++ bloco-4/dia3/exercicio1.js
@@ -0,0 +1,15 @@
+/*O fatorial é representado pelo sinal !*/
+
+/*4! = 4 x 3 x 2 x 1 = 24*/
+
+let fatorial = 1
+
+for (let index = 10; index > 1; index -= 1) {
+    fatorial *= index;
+
+    
+}
+console.log(fatorial)
+
+
+
