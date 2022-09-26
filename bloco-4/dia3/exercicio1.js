79d951e67fe04830b8a4466af2bdd0913d094fe0
tiagotk360 <contato@tk360.com.br>
2022-09-26 14:57:34 -0300

    exercicio inverte trybe

--- bloco-4/dia3/exercicio1.js
+++ bloco-4/dia3/exercicio1.js
@@ -2,14 +2,24 @@
 
 /*4! = 4 x 3 x 2 x 1 = 24*/
 
-let fatorial = 1
+/*let fatorial = 1
 
 for (let index = 10; index > 1; index -= 1) {
     fatorial *= index;
 
     
 }
-console.log(fatorial)
+console.log(fatorial)*/
 
+
+/* 2º exercicio*/
+
+let word = 'trybe';
+
+
+for (let index = 4; index >= 0; index -= 1) {
+    console.log(word[index]);
+     
+}
 
 
